1b7a992005dc98e04a9597aed40110f47fb707f2
MAVRICK-1 <mavrickrishi@gmail.com>
2024-12-01 17:40:09 +0530

    deploy it in ngrok

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 # Configurations for GitHub, Slack, and Kestra
 import os
 
-SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 KESTRA_API_URL = os.getenv("KESTRA_API_URL")
